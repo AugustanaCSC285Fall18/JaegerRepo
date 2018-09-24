811669e0b6865567e7c25f2e8386b6153305c784
Tiffany <hainguyen17@augustana.edu>
2018-09-24 12:14:20 -0500

    Added new ProjectData and TimePoint class

---
+++ ProjectJaeger/src/edu/augustana/csc285/jaeger/ProjectData.java
@@ -0,0 +1,18 @@
+package edu.augustana.csc285.jaeger;
+
+import java.io.File;
+import java.util.List;
+
+public class ProjectData {
+	private List<AnimalTrack> tracks;
+	private Video video;
+	
+	
+	public void exportCSVFile(File outFile) {
+		
+	}
+	
+	public void saveProject(File projectFile) {
+		
+	}
+}
---
+++ ProjectJaeger/src/edu/augustana/csc285/jaeger/TimePoint.java
@@ -0,0 +1,11 @@
+package edu.augustana.csc285.jaeger;
+
+import java.awt.Point;
+
+public class TimePoint {
+
+	
+	private Point pt;
+	private int FrameNum;
+
+}
