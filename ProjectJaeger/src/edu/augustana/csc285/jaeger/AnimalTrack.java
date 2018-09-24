bd5573056f879fe400ceba809a4c27b98bf5795f
Tiffany <hainguyen17@augustana.edu>
2018-09-24 12:07:38 -0500

    Uploaded initial ProjectData, AnimalTrack and TimePoint class

---
+++ ProjectJaeger/src/edu/augustana/csc285/jaeger/AnimalTrack.java
@@ -0,0 +1,13 @@
+package edu.augustana.csc285.jaeger;
+
+import java.util.List;
+
+public class AnimalTrack {
+
+	private String AnimalID;
+	private List<TimePoint> positions;
+	
+	public AnimalTrack() {
+		
+	}
+}
