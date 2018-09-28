da64eb07c33b2e70c77ea923c656168957d8e659
Tiffany <hainguyen17@augustana.edu>
2018-09-28 11:40:24 -0500

    Fixed bugs in the Main Window Controller

---
+++ ProjectJaeger/src/application/Main.java
@@ -0,0 +1,37 @@
+package application;
+	
+import org.opencv.core.Core;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+			BorderPane root = (BorderPane)loader.load();
+			MainWindowController controller = loader.getController();
+			
+			Scene scene = new Scene(root,root.getPrefWidth(),root.getPrefHeight());
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			//controller.initializeWithStage(primaryStage);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		launch(args);
+	}
+}
