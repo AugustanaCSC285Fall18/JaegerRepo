594374a3cfa46c48b0b5ae45cd4c8f48f254dc9d
minhta16 <minhta16@augustana.edu>
2018-09-30 23:17:49 -0400

    loading

--- ProjectJaeger/src/application/MainWindowController.java
+++ ProjectJaeger/src/application/MainWindowController.java
@@ -31,6 +31,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Path;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -39,7 +40,6 @@ import utils.UtilsForOpenCV;
 
 
 public class MainWindowController {
-	
 	
 	@FXML	private Button btnBrowse;
 	@FXML	private ImageView videoView;
@@ -51,20 +51,34 @@ public class MainWindowController {
 
 	private ProjectData project;
 	private Stage stage;
+	private boolean videoPlaying;
 	
 
 	
-@FXML public void initialize() {
-		//loadVideo("S:/class/cs/285/sample_videos/sample1.mp4");		
+	@FXML
+	public void initialize() {
+	
+		//TODO: delete this after done debugging.
+//		loadVideo("assets/sample1.mp4");	
+		videoPlaying = false;
 		sliderVideoTime.valueProperty().addListener((obs, oldV, newV) -> showFrameAt(newV.intValue())); 
 		
-		AnimalTrack trialChick = new AnimalTrack("Chick 1");
-		
 		GraphicsContext pathGc = pathCanvas.getGraphicsContext2D();
+		
+		int currentTrack = 0;
+		int ovalDiameter = 6;
+		int frameAdd = 5;
 		pathCanvas.setOnMousePressed(new EventHandler<MouseEvent>() {
 		    public void handle(MouseEvent me) {
-		        pathGc.fillOval(me.getX(), me.getY(), 10, 10);
-		        trialChick.add(new TimePoint(me.getX(), me.getY(), (int) sliderVideoTime.getValue()));
+		        pathGc.fillOval(me.getX(), me.getY(), ovalDiameter, ovalDiameter);
+		        if (project.getTracks().get(currentTrack).getTimePoints().size() != 0) {
+		        	double lastX = project.getTracks().get(currentTrack).getFinalTimePoint().getX() + ovalDiameter / 2;
+		        	double lastY = project.getTracks().get(currentTrack).getFinalTimePoint().getY() + ovalDiameter / 2;
+		        	pathGc.setLineWidth(2);
+		        	pathGc.strokeLine(lastX, lastY, me.getX() + ovalDiameter / 2, me.getY() + ovalDiameter / 2);
+		        }
+		        project.getTracks().get(currentTrack).add(new TimePoint(me.getX(), me.getY(), (int) sliderVideoTime.getValue()));
+		        showFrameAt(Integer.parseInt(textFieldCurFrameNum.getText()) + frameAdd);
 		        System.out.println("Mouse pressed: " + me.getX() + " , " + me.getY() + " at frame:" + (int) sliderVideoTime.getValue());
 		    }
 		});
@@ -74,14 +88,14 @@ public class MainWindowController {
 		}
 	}
 
-public void initializeWithStage(Stage stage) {
-	this.stage = stage;
+	public void initializeWithStage(Stage stage) {
+		this.stage = stage;
+		
+		// bind it so whenever the Scene changes width, the videoView matches it
+		// (not perfect though... visual problems if the height gets too large.)
+		//videoView.fitWidthProperty().bind(videoView.getScene().widthProperty());
 	
-	// bind it so whenever the Scene changes width, the videoView matches it
-	// (not perfect though... visual problems if the height gets too large.)
-	//videoView.fitWidthProperty().bind(videoView.getScene().widthProperty());
-
-}
+	}
 
 	@FXML
 	public void handleBrowse()  {
@@ -90,8 +104,19 @@ public void initializeWithStage(Stage stage) {
 		File chosenFile = fileChooser.showOpenDialog(stage);
 		if (chosenFile != null) {
 			loadVideo(chosenFile.getPath());
+			System.err.println(chosenFile.getPath());
 		}		
 		
+		//vidCanvas.widthProperty().bind(videoView.getScene().widthProperty());
+
+	}
+	
+	@FXML
+	public void handlePlay()  {
+		if (project.getVideo() != null) {
+			videoPlaying = true;
+			playVideo();
+		}
 		//vidCanvas.widthProperty().bind(videoView.getScene().widthProperty());
 
 	}
@@ -99,9 +124,17 @@ public void initializeWithStage(Stage stage) {
 	public void loadVideo(String filePath) {
 		try {
 			project = new ProjectData(filePath);
+			project.getTracks().add(new AnimalTrack("Chick 1"));
 			Video video = project.getVideo();
 			sliderVideoTime.setMax(video.getTotalNumFrames()-1);
+
+			// set current videocanvas & overlay to the size of the video
+			Image curFrame = UtilsForOpenCV.matToJavaFXImage(project.getVideo().readFrame());
+			vidCanvas.setHeight(vidCanvas.getWidth()*vidCanvas.getHeight()/curFrame.getWidth());
+			pathCanvas.setWidth(vidCanvas.getWidth());
+			pathCanvas.setHeight(vidCanvas.getHeight());
 			showFrameAt(0);
+			
 		
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -113,16 +146,28 @@ public void initializeWithStage(Stage stage) {
 		project.getVideo().setCurrentFrameNum(frameNum);
 		Image curFrame = UtilsForOpenCV.matToJavaFXImage(project.getVideo().readFrame());
 		GraphicsContext vidGc = vidCanvas.getGraphicsContext2D();
-		vidGc.drawImage(curFrame, 0, 0, vidCanvas.getWidth(), vidCanvas.getWidth()*vidCanvas.getHeight()/curFrame.getWidth());
-		 
-		pathCanvas.setWidth(vidCanvas.getWidth());
-		pathCanvas.setHeight(vidCanvas.getWidth()*vidCanvas.getHeight()/curFrame.getWidth() - 20);
+		GraphicsContext pathGc = pathCanvas.getGraphicsContext2D();
+		vidGc.drawImage(curFrame, 0, 0, vidCanvas.getWidth(), vidCanvas.getHeight());
+		pathGc.clearRect(0, 0, pathCanvas.getWidth(), pathCanvas.getHeight());
+		drawPath(0, frameNum);
 
 		textFieldCurFrameNum.setText(String.format("%05d",frameNum));	
 		
 	}
 	
+	private void drawPath(int trackNum, int frameNum) {
+		GraphicsContext vidGc = pathCanvas.getGraphicsContext2D();
+		vidGc.clearRect(0, 0, pathCanvas.getWidth(), pathCanvas.getHeight());
+		for(TimePoint tp : project.getTracks().get(trackNum).getTimePoints()) {
+	        vidGc.fillOval(tp.getX(), tp.getY(), 3, 3);
+		}
+	}
 	
-	
+	private void playVideo() {
+		while (project.getVideo().getCurrentFrameNum() <= project.getVideo().getEndFrameNum() && videoPlaying) {
+			project.getVideo().setCurrentFrameNum(project.getVideo().getCurrentFrameNum() + 1);
+			// TODO: complete this
+		}
+	}
 	
 }
