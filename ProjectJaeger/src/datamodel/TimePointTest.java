777bd59c25b4e5d3706d19e4158ba9cb15a2d898
tuannguyen17 <tuannguyen17@augustana.edu>
2018-10-09 17:32:55 -0500

    Added the methods to test the getDistanceTo and testGetTimeDifferAfter methods in TimePointTest Class

--- ProjectJaeger/src/datamodel/TimePointTest.java
+++ ProjectJaeger/src/datamodel/TimePointTest.java
@@ -1,18 +1,31 @@
 package datamodel;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
-import static datamodel.TimePoint.*;
+
 import org.junit.jupiter.api.Test;
 
 class TimePointTest {
-	private double x=100;     // location
-	private double y=200;  
-	
+
+	TimePoint tp1 = new TimePoint(40, 120, 3244);
+	TimePoint tp2 = new TimePoint(100, 200, 456);
+	TimePoint tp3 = new TimePoint(325, 435, 245);
+
 	@Test
 	void testGetDistanceTo() {
-		TimePoint Other = new TimePoint(100,200,456);
-	//	assertEquals(3, TimePoint.getDistanceTo(Other));
+
+		assertEquals(100, tp1.getDistanceTo(tp2));
+		assertEquals(0, tp1.getDistanceTo(tp1));
+		assertEquals(Math.sqrt(180450), tp1.getDistanceTo(tp3));
+		assertEquals(Math.sqrt(105850), tp3.getDistanceTo(tp2));
+		
 	}
 
+	@Test
+	void testGetTimeDiffAfter() {
+
+		assertEquals(2788, tp1.getTimeDiffAfter(tp2));
+		assertEquals(0, tp1.getTimeDiffAfter(tp1));
+		assertEquals(-211, tp3.getTimeDiffAfter(tp2));
+		assertEquals(-2999, tp3.getTimeDiffAfter(tp1));
+	}
 }
